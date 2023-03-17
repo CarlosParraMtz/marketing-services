9b2479c73ad354d19e4d010e3e2d7080e51001f2
Carlos Parra <86031209+CarlosParraMtz@users.noreply.github.com>
2023-03-17 12:23:46 -0600

    Agregado google analitycs

--- pages/_app.js
+++ pages/_app.js
@@ -21,6 +21,19 @@ export default function App({ Component, pageProps }) {
 				<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
 				<link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@700&display=swap" rel="stylesheet" />
 				<link rel="icon" type="image/x-icon" href="/favicon.ico" />
+
+
+				<script async src="https://www.googletagmanager.com/gtag/js?id=G-3DT2ZFKH88"></script>
+				<script
+					dangerouslySetInnerHTML={{
+						__html: `
+					window.dataLayer = window.dataLayer || [];
+					function gtag(){dataLayer.push(arguments);}
+					gtag('js', new Date());
+					gtag('config', 'G-3DT2ZFKH88');
+					`}}
+				/>
+
 			</Head>
 
 			<AnimatePresence exitBeforeEnter >
